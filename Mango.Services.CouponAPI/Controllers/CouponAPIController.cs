86ffc7746113de08ffe4569afb9c93ba2695df6d
Mukesh Das Bherudas Vaishnav <mukesh.bherudas@kanerika.com>
2024-03-14 15:57:26 +0530

    Added endpoints for 'GetByCode', 'Put', 'Post', and 'Delete'

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -26,7 +26,7 @@ namespace Mango.Services.CouponAPI.Controllers
         {
             try
             {
-                IEnumerable<Coupon> objList = _db.Coupons.ToList(); 
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
                 _response.Result = _mapper.Map<IEnumerable<CouponDTO>>(objList);
             }
             catch (Exception ex)
@@ -43,10 +43,82 @@ namespace Mango.Services.CouponAPI.Controllers
         {
             try
             {
-                Coupon obj = _db.Coupons.FirstOrDefault(u=>u.CouponId == id);
+                Coupon obj = _db.Coupons.FirstOrDefault(u => u.CouponId == id);
                 _response.Result = _mapper.Map<CouponDTO>(obj);
             }
-            catch(Exception ex)
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDTO GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u => u.CouponCode.ToLower() == code.ToLower());  // throws exception in case obj is null which is handled in catch
+                // if(obj == null){_response.IsSuccess = false;}   // or do it in another check if using firstordefault
+                _response.Result = _mapper.Map<CouponDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDTO Post([FromBody] CouponDTO coupondto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(coupondto);
+                _db.Add(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDTO Put([FromBody] CouponDTO coupondto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(coupondto);
+                _db.Update(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDTO Delete([FromBody] int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u => u.CouponId == id);
+                _db.Coupons.Remove(obj);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
             {
                 _response.IsSuccess = false;
                 _response.Message = ex.Message;
