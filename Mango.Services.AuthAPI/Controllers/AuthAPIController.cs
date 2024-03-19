20041afa3a8a0a53e6f3033b5d99905fdacead27
Mukesh Das Bherudas Vaishnav <mukesh.bherudas@kanerika.com>
2024-03-19 13:35:59 +0530

    Added Auth Service for login and registration, consumed them in AuthAPIController

--- Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mango.Services.AuthAPI.Models.DTO;
+using Mango.Services.AuthAPI.Services.IService;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.AuthAPI.Controllers
@@ -7,18 +9,42 @@ namespace Mango.Services.AuthAPI.Controllers
     [ApiController]
     public class AuthAPIController : ControllerBase
     {
+        private readonly IAuthService _authService;
+        protected ResponseDTO _response;
+
+        public AuthAPIController(IAuthService authService)
+        {
+           _authService = authService;
+            _response = new();
+        }
+
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register()
+        public async Task<IActionResult> Register([FromBody] RegistrationRequestDTO model)
         {
-            return Ok();
+            var errorMessage = await _authService.Register(model);  // return success or failure error messages as register method returns string
+            if(!string.IsNullOrEmpty(errorMessage))
+            {
+                _response.IsSuccess = false;
+                _response.Message = errorMessage;
+                return BadRequest(_response);
+            }
+            return Ok(_response);   // new responsedto obj in case of no errors
         }
 
 
         [HttpPost("login")]
-        public async Task<IActionResult> login()
+        public async Task<IActionResult> login([FromBody] LoginRequestDTO model)
         {
-            return Ok();
+            var loginResponse = await _authService.Login(model);
+            if(loginResponse.User == null)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Username or Password is incorrect";
+                return BadRequest(_response);
+            }
+            _response.Result = loginResponse;
+            return Ok(_response);
         }
     }
 }
---
+++ Mango.Services.AuthAPI/Services/AuthService.cs
@@ -0,0 +1,92 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.DTO;
+using Mango.Services.AuthAPI.Services.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDBContext _db;
+        // for identity helper methods to take care of all auth related complexity, like creating user, assigning roles, securing passwords, etc
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(ApplicationDBContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower());
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDTO.Password);
+
+            if(user == null || isValid == false)
+            {
+                return new LoginResponseDTO() { User = null, Token = "" };
+            }
+
+            // if user is found, generate jwt token
+            UserDTO userDTO = new()
+            {
+                Email = user.Email,
+                Id = user.Id,
+                Name = user.Name,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                User = userDTO,
+                Token = ""
+            };
+
+            return loginResponseDTO;
+        }
+
+        public async Task<string> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            ApplicationUser user = new()   
+            {
+                UserName = registrationRequestDTO.Email,
+                Email = registrationRequestDTO.Email,
+                NormalizedEmail = registrationRequestDTO.Email.ToUpper(),
+                Name = registrationRequestDTO.Name,
+                PhoneNumber = registrationRequestDTO.PhoneNumber,
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDTO.Password);   // creates a user, with all heavylifting like password hashing, done internally
+                if(result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(u => u.UserName == registrationRequestDTO.Email);
+
+                    UserDTO userDto = new()
+                    {
+                        Email = userToReturn.Email,
+                        Name = userToReturn.Name,
+                        Id = userToReturn.Id,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+
+                    return "user created successfully";
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().Description;  // first error in the list of erros
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return "error encountered"; // in case of exceptions
+
+        }
+    }
+}
