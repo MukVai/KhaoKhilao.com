ad873297e8733baa0351326052692cedfd2ca8cc
Mukesh Das Bherudas Vaishnav <mukesh.bherudas@kanerika.com>
2024-03-21 13:39:58 +0530

    Added claim for role in AuthController and added individual asp-validations for each property in Views

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -48,7 +48,7 @@ namespace Mango.Web.Controllers
             }
             else
             {
-                ModelState.AddModelError("CustomEr<ror", responseDTO.Message);
+                ModelState.AddModelError("CustomError", responseDTO.Message);
                 return View(obj);
             }
         }
@@ -121,6 +121,8 @@ namespace Mango.Web.Controllers
 
             identity.AddClaim(new Claim(ClaimTypes.Name,
                 jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+            identity.AddClaim(new Claim(ClaimTypes.Role,
+                jwt.Claims.FirstOrDefault(u => u.Type == "role").Value));
 
             var principal = new ClaimsPrincipal(identity);
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
