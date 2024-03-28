6ec877e36c6d9baa126309e9c4d89fda5d3a4560
Mukesh Das Bherudas Vaishnav <mukesh.bherudas@kanerika.com>
2024-03-28 17:16:09 +0530

    Added Home Controller Logic, Details and Index Views

--- Mango.Web/Models/ProductDTO.cs
+++ Mango.Web/Models/ProductDTO.cs
@@ -10,5 +10,7 @@ namespace Mango.Web.Models
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageURL { get; set; }
+        [Range(1,100)]
+        public int Count { get; set; } = 1;
     }
 }
